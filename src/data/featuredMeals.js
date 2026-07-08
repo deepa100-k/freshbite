a2a99a8f2670cfff2b036c860853d96e9ecc7044
deepa <deepa100091@gmail.com>
2026-07-08 15:28:00 +0530

    fix: resolve assets using vite dynamic urls

--- src/data/featuredMeals.js
+++ src/data/featuredMeals.js
@@ -1,15 +1,15 @@
 // src/data/featuredMeals.js
 
-const avocadoToastImg = "/food-images/AvocadoGreensaladcard-1.jpg";
-const buddhaBowlImg    = "/food-images/Quinoa&BroccoliBowlcard-1.jpg";
-const fruitBowlImg      = "/food-images/fruitbowlscard-1.webp";
-const greenSmoothieImg  = "/food-images/DetoxGreenSmoothiecard-1.jpg";
+// Vite dynamic asset resolution helper
+const getAssetUrl = (name) => {
+  return new URL(`../assets/${name}`, import.meta.url).href;
+};
 
 export const featuredMeals = [
   {
     id: 1,
     name: "Avocado Toast",
-    image: avocadoToastImg,
+    image: getAssetUrl("AvocadoGreensaladcard-1.jpg"), // Aapke asset folder ka exact file name
     price: 299,
     rating: 4.9,
     calories: "280 Cal",
@@ -17,7 +17,7 @@ export const featuredMeals = [
   {
     id: 2,
     name: "Buddha Bowl",
-    image: buddhaBowlImg,
+    image: getAssetUrl("Quinoa&BroccoliBowlcard-1.jpg"),
     price: 399,
     rating: 4.8,
     calories: "350 Cal",
@@ -25,7 +25,7 @@ export const featuredMeals = [
   {
     id: 3,
     name: "Fruit Bowl",
-    image: fruitBowlImg,
+    image: getAssetUrl("fruitbowlscard-1.webp"),
     price: 249,
     rating: 5.0,
     calories: "210 Cal",
@@ -33,7 +33,7 @@ export const featuredMeals = [
   {
     id: 4,
     name: "Green Smoothie",
-    image: greenSmoothieImg,
+    image: getAssetUrl("DetoxGreenSmoothiecard-1.jpg"),
     price: 199,
     rating: 4.7,
     calories: "180 Cal",
